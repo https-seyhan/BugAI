085c76b12f2f28a70c94ea9c94d7809ea2f02931
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-01-02 19:11:55 +1100

    Update AIBugCovClass.py

--- CovClass/AIBugCovClass.py
+++ CovClass/AIBugCovClass.py
@@ -4,7 +4,6 @@ from sklearn.covariance import EllipticEnvelope # An object for detecting outlie
 from sklearn.ensemble import IsolationForest # Isolation Forest Algorithm
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
-from sklearn import metrics
 #Import scikit-learn dataset library
 from sklearn import datasets
 from gensim.models import Word2Vec
