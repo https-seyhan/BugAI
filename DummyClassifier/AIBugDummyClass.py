a335bb2f6f55f28b81b816a192826e60125bef30
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-01-16 17:33:40 +1100

    Update AIBugDummyClass.py

--- DummyClassifier/AIBugDummyClass.py
+++ DummyClassifier/AIBugDummyClass.py
@@ -53,15 +53,6 @@ def convertcbow(dataset):
                 pass
 
         vectorised_codes.append(linecode)
-    #print("Line Code", array(linecode).shape)
-    # print(linecode)
-
-    # print('Line codes ', linecode)
-    # print('Vectorised Codes ', vectorised_codes[0])
-    # print('Vectorised Codes ', len(vectorised_codes))
-    # print(f'Sentences: {sentences}')
-    #print("Vectorised Codes ", array(vectorised_codes).shape)
-    return vectorised_codes
 
 def pad_trunc(data, maxlen):
     new_data = []
