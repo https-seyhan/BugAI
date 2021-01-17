b5b473b47709bce2b735c3b7cdc960ae5accf8d6
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-01-17 14:17:35 +1100

    Update AIBugDummyClass.py

--- DummyClassifier/AIBugDummyClass.py
+++ DummyClassifier/AIBugDummyClass.py
@@ -26,9 +26,7 @@ def convertcbow(dataset):
     
     print(' CBOW model ', cbowmodel)
 
-    # Test cbow model
-    #print("Test CBOW on the data")
-    #print(cbowmodel['eclipse'])
+
 
     classes = dataset['classname']
 
