0dc4efce2d85616b0591e6eb8508c0639c87329f
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-01-20 08:29:58 +1100

    Update BugAI_LSTM_oneclass.py

--- LSTM/BugAI_LSTM_oneclass.py
+++ LSTM/BugAI_LSTM_oneclass.py
@@ -28,7 +28,6 @@ def getcbow(dataset):
 
     sentences = []
     vectorised_codes = []
-
     ast = [row.split('::') for row in dataset['classname']]
     #print('ASTs ', ast[:2])
     #the imput to the cbow is list of list of each line
