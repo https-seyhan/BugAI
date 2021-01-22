254d4606c7ca1eccd48c2816372f42072ee74656
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-01-23 08:45:18 +1100

    Update AIBugForestClass.py

--- Forest/AIBugForestClass.py
+++ Forest/AIBugForestClass.py
@@ -25,7 +25,7 @@ def convertcbow(dataset):
     print("Cbow called")
 
     ast = [row.split('::') for row in dataset['classname']]
-    # print('ASTs ', ast[:2])
+   
     # the input to the cbow is list of list of each line
     # size of the word vector of a given token must be equal to embedding_dim of the LSTM model
     cbowmodel = Word2Vec(ast, min_count=1, size=embedding_dims, workers=3, window=6, sg=0)
