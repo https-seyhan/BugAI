b3044bd87733e3629014e1a3514100b71b3acb9a
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-15 11:53:57 +1100

    Update AIBugCovClass.py

--- CovClass/AIBugCovClass.py
+++ CovClass/AIBugCovClass.py
@@ -72,7 +72,6 @@ def getDataset():
 
 def OneClassModel(vectorised_data, target):
     print("SVM model is called ")
-
     split_point = int(len(vectorised_data) * .7)
     print('Split Point ', split_point)
     
