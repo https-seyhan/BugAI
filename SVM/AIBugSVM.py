a1d42d9fc36efe8f8a3122d1784bfa593bd811f7
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-23 09:54:47 +1100

    Update AIBugSVM.py

--- SVM/AIBugSVM.py
+++ SVM/AIBugSVM.py
@@ -22,7 +22,7 @@ def convertcbow(dataset):
     # the input to the cbow is list of list of each line
     # size of the word vector of a given token must be equal to embedding_dim of the LSTM model
     cbowmodel = Word2Vec(ast, min_count=1, size=embedding_dims, workers=3, window=3, sg=0)
-    # print(ast[:2])
+    
     print(' CBOW model ', cbowmodel)
     # Test cbow model
     classes = dataset['classname']
