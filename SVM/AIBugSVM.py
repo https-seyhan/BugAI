57fffee46fa93ad5e6ad4dcad36417ffc42b1bf7
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-23 10:03:48 +1100

    Update AIBugSVM.py

--- SVM/AIBugSVM.py
+++ SVM/AIBugSVM.py
@@ -74,7 +74,6 @@ def getDataset():
 
 def SVMModel(vectorised_data, target):
     print("SVM model is called ")
-
     split_point = int(len(vectorised_data) * .7)
     print('Split Point ', split_point)
 
