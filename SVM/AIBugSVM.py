a79901988aa934cfe3e5631c4ba0e89674f20bab
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-23 09:49:12 +1100

    Update AIBugSVM.py

--- SVM/AIBugSVM.py
+++ SVM/AIBugSVM.py
@@ -35,7 +35,7 @@ def convertcbow(dataset):
     for codes in classes:
         linecode = []
         tokens = codes.split('::')
-        # print(tokens)
+        
         sentences.append(tokens)
         for token in tokens:
             try:
