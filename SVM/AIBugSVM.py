a69d43ba8b6078f5a7189f06f37b59457aa48aa8
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-23 09:45:20 +1100

    Update AIBugSVM.py

--- SVM/AIBugSVM.py
+++ SVM/AIBugSVM.py
@@ -18,8 +18,7 @@ def convertcbow(dataset):
     vectorised_codes = []
     print("Cbow called")
 
-    # bugs = pd.read_csv('bug-metrics.csv', sep= ',')
-    
+    # bugs = pd.read_csv('bug-metrics.csv', sep= ',')  
 
     ast = [row.split('::') for row in dataset['classname']]
     # print('ASTs ', ast[:2])
