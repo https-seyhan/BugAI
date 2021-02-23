c6cda9f93ae74e54e8672be52a5b622630fc8fee
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-24 09:19:05 +1100

    Update AIBugOutlinerClass.py

--- OutLinerClassification/AIBugOutlinerClass.py
+++ OutLinerClassification/AIBugOutlinerClass.py
@@ -93,7 +93,6 @@ def OutlinerModel(vectorised_data, target):
     x_test = vectorised_data[split_point:]
     y_test = target[split_point:]
 
-
     #make each point of data of uniform lenght
     x_train = pad_trunc(x_train, maxlen)
     x_test = pad_trunc(x_test, maxlen)
