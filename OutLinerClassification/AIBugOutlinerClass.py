d9405c05d15fade9fa5991cbebe5ff16647c84fc
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-24 09:24:20 +1100

    Update AIBugOutlinerClass.py

--- OutLinerClassification/AIBugOutlinerClass.py
+++ OutLinerClassification/AIBugOutlinerClass.py
@@ -126,6 +126,7 @@ def OutlinerModel(vectorised_data, target):
     # print("Confusion matrix:\n{}".format(confusion_matrix(y_test.argmax(axis=1), pred.argmax(axis=1))))
     print("Confusion matrix:\n{}".format(confusion_matrix(np.array(y_test), pred)))
     print(classification_report(y_test, pred))
+
 if __name__ == '__main__':
 
     dataset = getDataset()
