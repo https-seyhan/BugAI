214239aaa8db93c9c5faf3fbcd39e9b75ac0423f
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-01 16:40:25 +1100

    Update AIBugCovClass.py

--- CovClass/AIBugCovClass.py
+++ CovClass/AIBugCovClass.py
@@ -1,3 +1,5 @@
+import  pandas as pd
+import numpy as np
 from sklearn import svm
 from sklearn.dummy import DummyClassifier 
 from sklearn.covariance import EllipticEnvelope # An object for detecting outliers in a Gaussian distributed dataset
@@ -7,8 +9,7 @@ from sklearn.metrics import confusion_matrix, accuracy_score, classification_rep
 #Import scikit-learn dataset library
 from sklearn import datasets
 from gensim.models import Word2Vec
-import  pandas as pd
-import numpy as np
+
 from numpy import array
 from keras.utils import to_categorical
 import seaborn as sb
