70187adcd66e67865f149324e0201ade07b01db0
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-03 13:47:41 +1100

    Update AIBugRFC.py

--- Forest/AIBugRFC.py
+++ Forest/AIBugRFC.py
@@ -70,7 +70,6 @@ def collect_expected(dataset):
     expected = []
     #bugsdata = pd.read_csv('bug-metrics.csv', sep= ',')
     #print(dataset.columns)
-
     bugs = dataset['criticalBugs'] # training dataset has 8 critical bugs and test dataset has 2. Extreamly unbalanced dataset.
 
     for bug in bugs:
