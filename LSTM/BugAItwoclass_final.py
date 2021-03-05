67d1eaba054b404f1a2579be700f2b3cc392d166
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-05 12:44:39 +1100

    Update BugAItwoclass_final.py

--- LSTM/BugAItwoclass_final.py
+++ LSTM/BugAItwoclass_final.py
@@ -6,7 +6,6 @@ from keras.layers import Dense, Activation, Dropout, Flatten, Embedding, LSTM, B
 from keras.optimizers import RMSprop
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
-
 from random import shuffle, sample
 from gensim.models import Word2Vec
 from random import shuffle, sample
