115bfaa23f172d9fa30e08f2a558dd54b7cf8643
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-05 13:08:36 +1100

    Update BugAItwoclass_final.py

--- LSTM/BugAItwoclass_final.py
+++ LSTM/BugAItwoclass_final.py
@@ -202,8 +202,7 @@ def getmaxlen(dataset):
     for codes in classes:
         linecode = []
         tokens = codes.split('::')
-        #print(len(tokens))
-        
+        #print(len(tokens))       
 def plotresults(history, y_train):
 
     weights = compute_sample_weight(class_weight="balanced", y=y_train)
