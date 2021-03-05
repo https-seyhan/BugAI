13ae4e77270145f4b03dc9f1cbd8a87b8d7aa11b
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-05 12:49:57 +1100

    Update BugAItwoclass_final.py

--- LSTM/BugAItwoclass_final.py
+++ LSTM/BugAItwoclass_final.py
@@ -34,7 +34,6 @@ def getcbow(dataset):
     vectorised_codes = []
     print("Cbow called")
     ast = [row.split('::') for row in dataset['classname']]
-    
     #the imput to the cbow is list of list of each line
     #size of the word vector of a given token must be equal to embedding_dim of the LSTM model
     cbowmodel = Word2Vec(ast, min_count=1, size= embedding_dims, workers=3, window=3, sg=0)
