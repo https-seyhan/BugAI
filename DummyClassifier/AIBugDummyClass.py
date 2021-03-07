56c880798999d2fc6fd95e0031ee9dacca67d94b
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-07 21:22:35 +1100

    Update AIBugDummyClass.py

--- DummyClassifier/AIBugDummyClass.py
+++ DummyClassifier/AIBugDummyClass.py
@@ -119,7 +119,6 @@ def DummyModel(vectorised_data, target):
 
     # print("Confusion matrix:\n{}".format(confusion_matrix(y_test.argmax(axis=1), pred.argmax(axis=1))))
     print("Confusion matrix:\n{}".format(confusion_matrix(np.array(y_test), pred)))
-
     print(classification_report(y_test, pred))
 
 
