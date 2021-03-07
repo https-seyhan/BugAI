7ec75cf5c2b17cd6f24f82cff2bbf19937ed0678
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-07 21:25:15 +1100

    Update AIBugDummyClass.py

--- DummyClassifier/AIBugDummyClass.py
+++ DummyClassifier/AIBugDummyClass.py
@@ -123,7 +123,7 @@ def DummyModel(vectorised_data, target):
 if __name__ == '__main__':
     dataset = getDataset()
     vectorised_data = convertcbow(dataset)
-    print(f'Vectorised Data Type {type(vectorised_data)}')
+    
     target = collect_expected(dataset)  # Biased two classes {198, 2} lenght is 200
 
     DummyModel(vectorised_data, target)
