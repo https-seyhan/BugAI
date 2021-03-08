8e883ee7a4e9115a2c72d714a22294e1ae19f9b5
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-09 10:20:09 +1100

    Update AIBugSVM.py

--- SVM/AIBugSVM.py
+++ SVM/AIBugSVM.py
@@ -117,7 +117,6 @@ def SVMModel(vectorised_data, target):
     print(classification_report(y_test, pred))
 
 if __name__ == '__main__':
-
     dataset = getDataset()
     vectorised_data = convertcbow(dataset)
     print(f'Vectorised Data Type {type(vectorised_data)}')
