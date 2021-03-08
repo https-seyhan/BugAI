400e68d4d37def2ee377f823a80c266601796047
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-09 10:18:13 +1100

    Update AIBugSVM.py

--- SVM/AIBugSVM.py
+++ SVM/AIBugSVM.py
@@ -117,8 +117,7 @@ def SVMModel(vectorised_data, target):
     print(classification_report(y_test, pred))
 
 if __name__ == '__main__':
-    # print the label type of cancer('malignant' 'benign')
-    #print("Labels: ", cancer.target_names)
+
 
     # print data(feature)shape
     print("Cancer Data Shape :", cancer.data.shape)
