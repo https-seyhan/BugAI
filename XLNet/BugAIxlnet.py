b7ac29230f23c7facd5784e1e2c221a049c23107
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-11 20:59:14 +1100

    Update BugAIxlnet.py

--- XLNet/BugAIxlnet.py
+++ XLNet/BugAIxlnet.py
@@ -30,7 +30,7 @@ df_data = pd.read_csv(data_file_address,sep=",",encoding="utf-8")
 print(len(df_data))
 
 df_data.criticalBugs.head(n=2)
-print(df_data.classname.head(n=2))
+
 
 df_data.criticalBugs.unique()
 # Analyse the labels distribution
