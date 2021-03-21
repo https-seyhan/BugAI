a3e6c2dca9efdb9044d33defcca4edb000b67289
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-21 16:56:54 +1100

    Update AIBug_EllipticEnvelope.py

--- IsolationForest/AIBug_EllipticEnvelope.py
+++ IsolationForest/AIBug_EllipticEnvelope.py
@@ -91,7 +91,7 @@ def CovModel(vectorised_data, target):
     #print("x_train shapes :", nsamples, nx, ny)
     x_train = np.reshape(x_train, (nsamples, nx * ny))
     #x_train = np.reshape(x_train, (len(x_train), maxlen, embedding_dims))
-    #print("Reshape of X Train :", x_train.shape)
+    
 
     nsamples, nx, ny = array(x_test).shape
     print("x_test shapes :", nsamples, nx, ny)
