91a40d550cac726a460b5ff0e5694f54943f0b1c
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-31 21:50:43 +1100

    Update AIBug_DummyClass.py

--- DummyClassifier/AIBug_DummyClass.py
+++ DummyClassifier/AIBug_DummyClass.py
@@ -24,7 +24,6 @@ def convertcbow(dataset):
     
     print(' CBOW model ', cbowmodel)
     classes = dataset['classname']
-
     for codes in classes:
         linecode = []
         tokens = codes.split('::')
