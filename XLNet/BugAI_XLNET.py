fe470c6561fb11646d89683d8e5db32c4280205a
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-04-10 09:56:50 +1000

    Update BugAI_XLNET.py

--- XLNet/BugAI_XLNET.py
+++ XLNet/BugAI_XLNET.py
@@ -23,7 +23,6 @@ from pytorch_transformers import (XLNetConfig, XLNetForSequenceClassification, X
 
 os.chdir('/home/saul/bugpred')
 data_file_address = "bug-metrics.csv"
-
 #df_data = pd.read_csv(data_file_address,sep=",",encoding="utf-8",names=['labels','texts'])
 df_data = pd.read_csv(data_file_address,sep=",",encoding="utf-8")
 df_data.criticalBugs.head(n=2)
