a9532c3bbb2b3c1a30dd2b3eccef355bc5480a9d
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-05-01 08:45:50 +1000

    Update AIBugCovClass.py

--- CovClass/AIBugCovClass.py
+++ CovClass/AIBugCovClass.py
@@ -77,7 +77,6 @@ def CovModel(vectorised_data, target):
     #plt.show()
     x_test = vectorised_data[split_point:]
     y_test = target[split_point:]
-
     #make each point of data of uniform lenght
     x_train = pad_trunc(x_train, maxlen)
     x_test = pad_trunc(x_test, maxlen)
