8d846c85389e23f2479cbf2a6136d2068b711d99
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-05-01 08:52:03 +1000

    Update AIBugCovClass.py

--- CovClass/AIBugCovClass.py
+++ CovClass/AIBugCovClass.py
@@ -79,7 +79,6 @@ def CovModel(vectorised_data, target):
     #make each point of data of uniform lenght
     x_train = pad_trunc(x_train, maxlen)
     x_test = pad_trunc(x_test, maxlen)
-
     nsamples, nx, ny = array(x_train).shape
     x_train = np.reshape(x_train, (nsamples, nx * ny))
     #x_train = np.reshape(x_train, (len(x_train), maxlen, embedding_dims))
