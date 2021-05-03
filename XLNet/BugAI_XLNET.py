90051a6ce1b4172fa9dcaa0f8a1f776d2f84afe0
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-05-03 21:04:19 +1000

    Update BugAI_XLNET.py

--- XLNet/BugAI_XLNET.py
+++ XLNet/BugAI_XLNET.py
@@ -125,7 +125,7 @@ for i,sentence in enumerate(sentences):
     # Add <cls> token
     tokens.append(CLS_ID)
     segment_ids.append(SEG_ID_CLS)
-   
+  
     input_ids = tokens
     
     # The mask has 0 for real tokens and 1 for padding tokens. Only real
