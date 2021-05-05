29b10b28bf6ebd6fac2f69dee9b99647b67bd17a
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-05-05 20:59:32 +1000

    Update BugAI_XLNET.py

--- XLNet/BugAI_XLNET.py
+++ XLNet/BugAI_XLNET.py
@@ -225,7 +225,7 @@ if FULL_FINETUNING:
     ]
 else:
     # Only fine tune classifier parameters
-    param_optimizer = list(model.classifier.named_parameters()) 
+    param_optimizer = list(model.classifier.named_parameters())
     optimizer_grouped_parameters = [{"params": [p for n, p in param_optimizer]}]
 optimizer = Adam(optimizer_grouped_parameters, lr=3e-5)
 
