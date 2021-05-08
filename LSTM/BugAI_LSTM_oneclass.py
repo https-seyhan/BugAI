438cf245644ba8ef5528e15866af1308e0dac226
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-05-09 05:33:17 +1000

    Update BugAI_LSTM_oneclass.py

--- LSTM/BugAI_LSTM_oneclass.py
+++ LSTM/BugAI_LSTM_oneclass.py
@@ -82,7 +82,7 @@ def lstmModel(vectorised_data, target):
     x_test = pad_trunc(x_test, maxlen)
 
     #reshape data into a numpy structure
-    
+   
     print("X_TRAIN Reshape Started ")
     print(f' Training data Size: {len(x_train)}')
     print("Number of word tokens ", maxlen)
