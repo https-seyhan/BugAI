2be3bc89d69c30e12bf0f94dace9282023543138
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-05-10 21:28:03 +1000

    Update AIBug_SupportVectorMachines.py

--- SVM/AIBug_SupportVectorMachines.py
+++ SVM/AIBug_SupportVectorMachines.py
@@ -70,7 +70,6 @@ def SVMModel(vectorised_data, target):
     x_train = vectorised_data[:split_point]
     y_train = target[:split_point]
     #y_train = to_categorical(y_train, 2)
-
     x_test = vectorised_data[split_point:]
     y_test = target[split_point:]
 
