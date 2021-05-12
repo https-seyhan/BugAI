5fc0dc3e7eaf3100a06c7dbccd4ced37f73891e3
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-05-12 20:16:06 +1000

    Update BugAI_XLNET.py

--- XLNet/BugAI_XLNET.py
+++ XLNet/BugAI_XLNET.py
@@ -321,7 +321,6 @@ result = {'eval_loss': eval_loss,
                   'eval_accuracy': eval_accuracy,
                   'loss': loss}
 report = classification_report(y_pred=np.array(y_predict),y_true=np.array(y_true))
-
 # Save the report into file
 output_eval_file = os.path.join(xlnet_out_address, "eval_results.txt")
 with open(output_eval_file, "w") as writer:
