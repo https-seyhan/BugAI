f4a655ee3eb26380902fabf9b5f05bde57031e20
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-07-01 20:35:18 +1000

    Update Bug_AI_Twoclass_V2.py

--- LSTM/Bug_AI_Twoclass_V2.py
+++ LSTM/Bug_AI_Twoclass_V2.py
@@ -102,7 +102,7 @@ def lstmModel(vectorised_data, target):
     y_test = to_categorical(y_test, 2)
     #print(f'Y_TEST DATA: {y_test}')
     print((f'Y_TEST_DATA LENGHT{len(y_test)}'))
-   
+  
     model = Sequential()
     #model.add(Embedding(embedding_dims, batch_size))
     #model.add(Embedding(output_dim=512, input_dim=10000, input_length=100))
