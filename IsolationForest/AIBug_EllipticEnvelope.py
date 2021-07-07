13f2b00fdea5161fb9649f88f3ba6c41b35a6e6b
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-07-07 13:30:04 +1000

    Update AIBug_EllipticEnvelope.py

--- IsolationForest/AIBug_EllipticEnvelope.py
+++ IsolationForest/AIBug_EllipticEnvelope.py
@@ -2,7 +2,7 @@ import  pandas as pd
 import numpy as np
 import seaborn as sb
 from sklearn import svm
-from sklearn.covariance import EllipticEnvelope
+from sklearn.covariance import EllipticEnvelope # Isolation Forest Module in Python
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 from sklearn import metrics
