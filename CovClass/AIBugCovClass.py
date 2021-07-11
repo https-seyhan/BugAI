308f207a44477a98cb63f77e88a4897aae9883a0
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-07-12 07:55:21 +1000

    Update AIBugCovClass.py

--- CovClass/AIBugCovClass.py
+++ CovClass/AIBugCovClass.py
@@ -70,7 +70,7 @@ def CovModel(vectorised_data, target):
     # split data into training and testing
     x_train = vectorised_data[:split_point]
     y_train = target[:split_point]
-    
+   
     #y_train = to_categorical(y_train, 2)
     #plt.hist(x_train)
     #plt.show()
