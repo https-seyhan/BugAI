7624abf7e1397b197a6c1164e364a75168cc7926
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-07-11 12:23:13 +1000

    Update AIBug_IsolationForest.py

--- Forest/AIBug_IsolationForest.py
+++ Forest/AIBug_IsolationForest.py
@@ -81,7 +81,7 @@ def ForestModel(vectorised_data, target):
     # split data into training and testing
     x_train = vectorised_data[:split_point]
     y_train = target[:split_point]
-   
+  
     x_test = vectorised_data[split_point:]
     y_test = target[split_point:]
     #make each point of data of uniform lenght
