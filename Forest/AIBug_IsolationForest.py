e68a87c24dabb4cb61c248459daa251d00419533
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-07-11 12:52:19 +1000

    Update AIBug_IsolationForest.py

--- Forest/AIBug_IsolationForest.py
+++ Forest/AIBug_IsolationForest.py
@@ -91,7 +91,7 @@ def ForestModel(vectorised_data, target):
     nsamples, nx, ny = array(x_test).shape
     print("x_test shapes :", nsamples, nx, ny)
     x_test = np.reshape(x_test, (nsamples, nx * ny))
-   
+  
     n_outliers = int(outliers_fraction * nsamples)
     print("Number of Outliners :", n_outliers)
 
