e2ad412bdc461a32238a48bc7a836aa2b3be53b7
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-07-11 11:10:35 +1000

    Update AIBug_IsolationForest.py

--- Forest/AIBug_IsolationForest.py
+++ Forest/AIBug_IsolationForest.py
@@ -126,7 +126,7 @@ if __name__ == '__main__':
     vectorised_data = convertcbow(dataset)
     print(f'Vectorised Data Type {type(vectorised_data)}')
     target = collect_expected(dataset)  # Biased two classes {198, 2} lenght is 200
-  
+ 
     #DummyModel(vectorised_data, target)
     #CovModel(vectorised_data, target)
     ForestModel(vectorised_data, target)
