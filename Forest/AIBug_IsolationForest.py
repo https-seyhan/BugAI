7cc4e7d258bc1b733e15049243efc59f8a7c159a
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-07-11 11:04:01 +1000

    Update AIBug_IsolationForest.py

--- Forest/AIBug_IsolationForest.py
+++ Forest/AIBug_IsolationForest.py
@@ -71,7 +71,6 @@ def getDataset():
     dataset = pd.read_csv('bug-metrics.csv', sep= ',')
     return dataset
 
-
 def CovModel(vectorised_data, target):
     print(" EllipticEnvelope model is called ")
     split_point = int(len(vectorised_data) * .7)
