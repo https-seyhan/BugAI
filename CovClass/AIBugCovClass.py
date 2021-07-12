b07c23e2a3baf383d71aff6c40832382e3dda2fe
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-07-12 10:09:54 +1000

    Update AIBugCovClass.py

--- CovClass/AIBugCovClass.py
+++ CovClass/AIBugCovClass.py
@@ -96,7 +96,7 @@ def CovModel(vectorised_data, target):
   
     #print("Predictions :", pred, '\n')
     #print ("Actual :", np.array(y_test))
-    #y_test = to_categorical(y_test, 2)
+    
     # Model Accuracy: how often is the classifier correct?
     print("Accuracy: {:3f}".format(accuracy_score(y_test, pred > 0.5)))
     # print("Confusion matrix:\n{}".format(confusion_matrix(y_test.argmax(axis=1), pred.argmax(axis=1))))
