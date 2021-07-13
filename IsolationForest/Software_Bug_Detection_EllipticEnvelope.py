e2168d2884ed0db83e2037166c0e4cc1668da3b7
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-07-13 15:26:50 +1000

    Update Software_Bug_Detection_EllipticEnvelope.py

--- IsolationForest/Software_Bug_Detection_EllipticEnvelope.py
+++ IsolationForest/Software_Bug_Detection_EllipticEnvelope.py
@@ -71,7 +71,6 @@ def getDataset():
 def CovModel(vectorised_data, target):
     split_point = int(len(vectorised_data) * .7)
     print('Split Point ', split_point)
-
     # split data into training and testing
     x_train = vectorised_data[:split_point]
     y_train = target[:split_point]
