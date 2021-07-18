75efbf1a250b3277f91d21ed052da4f2af4605eb
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-07-18 13:33:38 +1000

    Update BugAItwoclass.py

--- LSTM/BugAItwoclass.py
+++ LSTM/BugAItwoclass.py
@@ -32,7 +32,7 @@ def getcbow(dataset):
     vectorised_codes = []
     print("Cbow called")
     #bugs = pd.read_csv('bug-metrics.csv', sep= ',')
-   
+  
     ast = [row.split('::') for row in dataset['classname']]
  
     #the input to the cbow is list of list of each line
