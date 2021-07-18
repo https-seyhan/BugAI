07c219724cf0a7cd7ffd108eeeaa689999968c4e
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-07-18 14:42:16 +1000

    Update BugAItwoclass.py

--- LSTM/BugAItwoclass.py
+++ LSTM/BugAItwoclass.py
@@ -52,7 +52,7 @@ def getcbow(dataset):
         sentences.append(tokens)
         for token in tokens:
             try:
-       
+      
                 #linecode.append(token)
                 #print("Word Vector ", len(cbowmodel[token]))
                 linecode.append(cbowmodel[token])
