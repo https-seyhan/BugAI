87fdaf90c0269ec7f327aaa7f9543ab2f21be7a2
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-07-21 12:05:47 +1000

    Update BugAI_TwoClass_final.py

--- LSTM/BugAI_TwoClass_final.py
+++ LSTM/BugAI_TwoClass_final.py
@@ -83,7 +83,7 @@ def lstmModel(vectorised_data, target):
     #make each point of data of uniform lenght
     x_train = pad_trunc(x_train, maxlen)
     x_test = pad_trunc(x_test, maxlen)
- 
+
     #reshape data into a numpy structure
 
     print("X_TRAIN Reshape Started ")
