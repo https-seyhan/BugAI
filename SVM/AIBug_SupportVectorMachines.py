005891812bfe201c2fa926d315213b457c5f7699
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-08-04 11:06:48 +1000

    Update AIBug_SupportVectorMachines.py

--- SVM/AIBug_SupportVectorMachines.py
+++ SVM/AIBug_SupportVectorMachines.py
@@ -17,7 +17,7 @@ def convertcbow(dataset):
     vectorised_codes = []
     print("Cbow called")
     ast = [row.split('::') for row in dataset['classname']]
-    # The input to the cbow is list of list of each line
+    #The input to the cbow is list of list of each line
     
     cbowmodel = Word2Vec(ast, min_count=1, size=embedding_dims, workers=3, window=3, sg=0)
     classes = dataset['classname']
