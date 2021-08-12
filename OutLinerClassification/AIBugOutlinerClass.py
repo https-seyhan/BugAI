ef911710df1517fd8c55ede6e51bf59eaaa3d836
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-08-12 10:51:42 +1000

    Update AIBugOutlinerClass.py

--- OutLinerClassification/AIBugOutlinerClass.py
+++ OutLinerClassification/AIBugOutlinerClass.py
@@ -19,7 +19,7 @@ def convertcbow(dataset):
     vectorised_codes = []
     print("Cbow called")
     ast = [row.split('::') for row in dataset['classname']]
-    # input of the cbow is list of each line
+    # input for the cbow is list of each line
     # size of the word vector of a given token must be equal to embedding_dim of the LocalOutlierFactor model
     cbowmodel = Word2Vec(ast, min_count=1, size=embedding_dims, workers=3, window=6, sg=0)
     print(' CBOW model ', cbowmodel)
