992efb81c36702bd78f85d16ad91b132f9e7ca3a
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-08-13 18:01:21 +1000

    Update BugAItwoclass.py

--- LSTM/BugAItwoclass.py
+++ LSTM/BugAItwoclass.py
@@ -97,7 +97,6 @@ def lstmModel(vectorised_data, target):
     print("Embedding Dims ", embedding_dims)
     #print(f'Training Data {x_train[:1]}')
     #print(type(x_train))
-
     #y_train = np.array(y_train)
     x_train = np.reshape(x_train, (len(x_train), maxlen, embedding_dims))
     print("X_TRAIN Reshape Completed ")
