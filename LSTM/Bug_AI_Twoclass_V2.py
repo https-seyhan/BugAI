873b47966925fe437f550ba755af0324b72e47d0
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-09-05 08:32:02 +1000

    Update Bug_AI_Twoclass_V2.py

--- LSTM/Bug_AI_Twoclass_V2.py
+++ LSTM/Bug_AI_Twoclass_V2.py
@@ -158,7 +158,7 @@ def fitmodel(model, x_train, y_train, x_test, y_test, batch_size, epochs):
               class_weight={0: 1., 1: 80.},
               #class_weight={0: 1., 1: 100.},
               validation_data=(x_test, y_test, val_sample_weights))
-
+    
     # model.fit(np.array(x_train), np.array(y_train),binary
     # batch_size = batch_size,
     # epochs =epochs,
