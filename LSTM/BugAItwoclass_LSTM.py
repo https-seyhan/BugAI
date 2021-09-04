1f5cf1edb9a804d645c872e7af685b50418bdbf2
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-09-04 10:39:02 +1000

    Update BugAItwoclass_LSTM.py

--- LSTM/BugAItwoclass_LSTM.py
+++ LSTM/BugAItwoclass_LSTM.py
@@ -47,7 +47,7 @@ def getcbow(dataset):
         tokens = codes.split('::')
         #print(tokens)
         sentences.append(tokens)
- 
+
         for token in tokens:
             try:
                 #linecode.append(token)
