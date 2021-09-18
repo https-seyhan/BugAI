9d065373c20982bf711e7da3c68e7471fc6a6266
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-09-18 12:44:47 +1000

    Update BugAI_Twoclass_LSTM.py

--- LSTM/BugAI_Twoclass_LSTM.py
+++ LSTM/BugAI_Twoclass_LSTM.py
@@ -185,7 +185,7 @@ def collect_expected(dataset):
     #print(dataset.columns)
     bugs = dataset['criticalBugs']
     for bug in bugs:
-        #print(bug)
+        
         expected.append(bug)
     return expected
 
