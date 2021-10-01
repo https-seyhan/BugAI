a653be7aeee5ef6528ee8d40caae2077089e57f6
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-10-01 10:58:10 +1000

    Update AIBugRFC.py

--- Forest/AIBugRFC.py
+++ Forest/AIBugRFC.py
@@ -104,7 +104,7 @@ def RFCModel(vectorised_data, target):
     pred = rfcmodel.predict(x_test)
     #print("Predictions :", pred, '\n')
     #print ("Actual :", np.array(y_test))
-
+    
     #y_test = to_categorical(y_test, 2)
     # Model Accuracy: how often is the classifier correct?
     #print("Accuracy:", metrics.accuracy_score(y_test, pred))
