0f29a75d47ee4806b23a7809b14f2cac4fa0122f
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-10-19 01:31:10 +1100

    Update AIBug_SupportVectorMachines.py

--- SVM/AIBug_SupportVectorMachines.py
+++ SVM/AIBug_SupportVectorMachines.py
@@ -69,7 +69,7 @@ def getDataset():
 def SVMModel(vectorised_data, target):
     split_point = int(len(vectorised_data) * .7)
     print('Split Point ', split_point)
-
+    
     # split data into training and testing
     x_train = vectorised_data[:split_point]
     y_train = target[:split_point]
