c3317ecb9540427c635af3e9ca3e79d652a7530f
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-11-02 01:21:07 +1100

    Update AIBug_SupportVectorMachines.py

--- SVM/AIBug_SupportVectorMachines.py
+++ SVM/AIBug_SupportVectorMachines.py
@@ -99,7 +99,7 @@ def SVMModel(vectorised_data, target):
     
     #print("Predictions :", pred, '\n')
     #print ("Actual :", np.array(y_test))
-    
+   
     #Model Accuracy: how often is the classifier correct?
     #print("Accuracy:", metrics.accuracy_score(y_test, pred))
     print("Accuracy: {:3f}".format(accuracy_score(y_test, pred > 0.5)))
