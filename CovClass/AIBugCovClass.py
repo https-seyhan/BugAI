f952b6f14e7c4026650826179f951aa0282e8a22
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-11-25 01:31:57 +1100

    Update AIBugCovClass.py

--- CovClass/AIBugCovClass.py
+++ CovClass/AIBugCovClass.py
@@ -89,7 +89,7 @@ def CovModel(vectorised_data, target):
     nsamples, nx, ny = array(x_test).shape
     print("x_test shapes :", nsamples, nx, ny)
     x_test = np.reshape(x_test, (nsamples, nx * ny))
-    #x_train = np.reshape(x_train, (len(x_train), maxlen, embedding_dims))
+    
     outliers_fraction =6/300
     n_outliers = int(outliers_fraction * nsamples)
     print("Number of Outliners :", n_outliers)
