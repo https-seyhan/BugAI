298da16fe86dc075f1a6ee8f1626b70941010023
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-11-28 10:20:02 +1100

    Update Detecting_Rare_Bugs_in_Software_codes_using_Isolation_Forest.py

--- Forest/Detecting_Rare_Bugs_in_Software_codes_using_Isolation_Forest.py
+++ Forest/Detecting_Rare_Bugs_in_Software_codes_using_Isolation_Forest.py
@@ -70,6 +70,7 @@ def pad_trunc(data, maxlen):
 def collect_expected(dataset):
     expected = []
     bugs = dataset['criticalBugs'] # training dataset has 8 critical bugs and test dataset has 2. Extremely unbalanced dataset.
+    
     for bug in bugs:
         expected.append(bug)
     return expected
