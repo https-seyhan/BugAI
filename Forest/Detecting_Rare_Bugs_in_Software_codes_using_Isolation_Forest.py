043f286ca61ed409ae9d875d14e3ea2a42a35add
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-11-28 10:07:32 +1100

    Update Detecting_Rare_Bugs_in_Software_codes_using_Isolation_Forest.py

--- Forest/Detecting_Rare_Bugs_in_Software_codes_using_Isolation_Forest.py
+++ Forest/Detecting_Rare_Bugs_in_Software_codes_using_Isolation_Forest.py
@@ -86,7 +86,7 @@ def ForestModel(vectorised_data, target):
     y_train = target[:split_point]
     x_test = vectorised_data[split_point:]
     y_test = target[split_point:]
-  
+ 
     #make each point of data of uniform lenght
     x_train = pad_trunc(x_train, maxlen)
     x_test = pad_trunc(x_test, maxlen)
