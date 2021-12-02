1a62ab8dd3c08cf0d8aea423480a7e26728e4da0
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-12-03 07:55:29 +1100

    Update BugAI_Twoclass_LSTM.py

--- LSTM/BugAI_Twoclass_LSTM.py
+++ LSTM/BugAI_Twoclass_LSTM.py
@@ -43,7 +43,7 @@ def getcbow(dataset):
     print("Test CBOW on the data")
     print(cbowmodel['eclipse'])
     classes = dataset['classname']
- 
+
     for codes in classes:
         linecode = []
         tokens = codes.split('::')
