5875de185faf87fbccb262c7858b6ed9641a7030
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-12-04 11:35:29 +1100

    Update AIBug_Support_Vector_Machines.py

--- SVM/AIBug_Support_Vector_Machines.py
+++ SVM/AIBug_Support_Vector_Machines.py
@@ -25,7 +25,7 @@ def convertcbow(dataset):
         linecode = []
         tokens = codes.split('::')
         sentences.append(tokens)
-  
+ 
         for token in tokens:
             try:          
                 linecode.append(cbowmodel[token])
