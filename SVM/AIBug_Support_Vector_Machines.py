1a6ebd12a06a4c14b06a3ad2c8e17d25a48ed127
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-12-05 10:38:10 +1100

    Update AIBug_Support_Vector_Machines.py

--- SVM/AIBug_Support_Vector_Machines.py
+++ SVM/AIBug_Support_Vector_Machines.py
@@ -94,7 +94,7 @@ def SVMModel(vectorised_data, target):
     # create SVM model
     #svmmodel = svm.SVC(kernel='poly', degree=8)
     #svmmodel = svm.SVC(kernel='sigmoid')
-    one_class_svmmodel = svm.OneClassSVM(nu=0.03, kernel="rbf", gamma=0.02)
+    one_class_svm_model = svm.OneClassSVM(nu=0.03, kernel="rbf", gamma=0.02)
     svmmodel.fit(x_train, y_train)
     pred = svmmodel.predict(x_test)
     
