37bdea5823846aa142c15938bbe8d8e431901e29
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-12-18 14:12:32 +1100

    Update BugAItwoclass_Bidirectional.py

--- LSTM/BugAItwoclass_Bidirectional.py
+++ LSTM/BugAItwoclass_Bidirectional.py
@@ -35,7 +35,7 @@ def getcbow(dataset):
     #print(bugs.columns)
     ast = [row.split('::') for row in dataset['classname']]
     #The input to the cbow is list of list of each line
-    #size of the word vector of a given token must be equal to embedding_dim of the LSTM model
+    #Size of the word vector of a given token must be equal to embedding_dim of the LSTM model
     cbowmodel = Word2Vec(ast, min_count=1, size= embedding_dims, workers=3, window=3, sg=0)
     print (' CBOW model ', cbowmodel)
     print(cbowmodel['eclipse'])
