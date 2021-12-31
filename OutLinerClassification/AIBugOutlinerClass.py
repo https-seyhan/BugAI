32b7b95d3684d1a7b9eafeb283ae5804a37d8f65
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-01-01 10:31:34 +1100

    Update AIBugOutlinerClass.py

--- OutLinerClassification/AIBugOutlinerClass.py
+++ OutLinerClassification/AIBugOutlinerClass.py
@@ -26,7 +26,7 @@ def convertcbow(dataset):
     print(' CBOW model ', cbowmodel)
     # Test cbow model 
     classes = dataset['classname']
-
+    
     for codes in classes:
         linecode = []
         tokens = codes.split('::')
