63b5f9fd3746a37658a59939a5ac64ef0845a9ea
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-01-24 11:09:19 +1100

    Update AIBugRFC.py

--- Forest/AIBugRFC.py
+++ Forest/AIBugRFC.py
@@ -83,7 +83,7 @@ def RFCModel(vectorised_data, target):
     
     x_test = vectorised_data[split_point:]
     y_test = target[split_point:]
-  
+ 
     #make each point of data of uniform lenght
     x_train = pad_trunc(x_train, maxlen)
     x_test = pad_trunc(x_test, maxlen)
