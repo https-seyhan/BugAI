5462156f7f3403e19379562081cc05dbb8d6c600
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-04-24 12:27:56 +1000

    Update AI_Bug_DummyClass.py

--- DummyClassifier/AI_Bug_DummyClass.py
+++ DummyClassifier/AI_Bug_DummyClass.py
@@ -59,7 +59,7 @@ def pad_trunc(data, maxlen):
 def collect_expected(dataset):
     expected = []
     bugs = dataset['criticalBugs'] # training dataset has 8 critical bugs and test dataset has 2. Extremely unbalanced dataset.
-  
+ 
     for bug in bugs:      
         expected.append(bug)
     return expected
