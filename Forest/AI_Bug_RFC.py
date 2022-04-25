0cf6b90f729bfa40294635a94d499b42a7802f27
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-04-25 11:29:05 +1000

    Update AI_Bug_RFC.py

--- Forest/AI_Bug_RFC.py
+++ Forest/AI_Bug_RFC.py
@@ -63,7 +63,7 @@ def pad_trunc(data, maxlen):
 
 def collect_expected(dataset):
     expected = []
-    
+   
     #bugsdata = pd.read_csv('bug-metrics.csv', sep= ',')
     bugs = dataset['criticalBugs'] # training dataset has 8 critical bugs and test dataset has 2. Extreamly unbalanced dataset.
     for bug in bugs:
