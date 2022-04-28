6c68ab95c6afc1ba06c17c3dc2e846ae2962d863
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-04-29 08:41:11 +1000

    Update AIBug_Support_Vector_Machines.py

--- SVM/AIBug_Support_Vector_Machines.py
+++ SVM/AIBug_Support_Vector_Machines.py
@@ -1,6 +1,6 @@
 import  pandas as pd
 import numpy as np
-from sklearn import svm
+from sklearn import svm #
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
