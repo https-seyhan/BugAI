6ccf6df11acb80100a8933c0d0bf9a9b0e2fb710
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-05-08 22:05:27 +1000

    Update AIBugCovClass.py

--- CovClass/AIBugCovClass.py
+++ CovClass/AIBugCovClass.py
@@ -20,7 +20,7 @@ def convertcbow(dataset):
     # the input to the cbow is list of list of each line
     cbowmodel = Word2Vec(ast, min_count=1, size=embedding_dims, workers=3, window=6, sg=0)
     print(' CBOW model ', cbowmodel)
-   
+  
     classes = dataset['classname']
     for codes in classes:
         linecode = []
