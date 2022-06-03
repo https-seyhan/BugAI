ed00327fd877158de4c90e0a8387ba5f94f4fdbf
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-06-04 08:41:54 +1000

    Update AIBugCovClass.py

--- CovClass/AIBugCovClass.py
+++ CovClass/AIBugCovClass.py
@@ -35,7 +35,7 @@ def convertcbow(dataset):
                 linecode.append(cbowmodel[token])
             except KeyError:
                 pass
-  
+ 
         vectorised_codes.append(linecode)
     return vectorised_codes
 
